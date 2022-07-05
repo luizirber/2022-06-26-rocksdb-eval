0342cf068435a020ef4abc018b50bb9e4ff6f4c3
Luiz Irber <contact+github@luizirber.org>
2022-07-04 18:09:33 -0700

    read from sigs

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,16 @@
 use std::collections::HashSet;
-use std::fs;
-use std::io;
-use std::path::Path;
+use std::path::{Path, PathBuf};
+use std::sync::atomic::{AtomicUsize, Ordering};
 use std::sync::Arc;
 
-use rkyv::{archived_root, ser::Serializer, Archive, Deserialize, Serialize};
-use rocksdb::{ColumnFamilyDescriptor, MergeOperands, Options, DB, DEFAULT_COLUMN_FAMILY_NAME};
+use byteorder::{LittleEndian, WriteBytesExt};
+use rkyv::{Archive, Deserialize, Serialize};
+use rocksdb::{MergeOperands, Options, DB};
 
-const SCRATCH_SIZE: usize = 256;
-type DefaultSerializer = rkyv::ser::serializers::AllocSerializer<SCRATCH_SIZE>;
-type DefaultDeserializer = rkyv::Infallible;
+use sourmash::signature::Signature;
+use sourmash::sketch::minhash::{max_hash_for_scaled, KmerMinHash};
+use sourmash::sketch::Sketch;
+
 type DatasetID = u64;
 
 fn merge_datasets(
@@ -17,18 +18,61 @@ fn merge_datasets(
     existing_val: Option<&[u8]>,
     operands: &MergeOperands,
 ) -> Option<Vec<u8>> {
-    let mut datasets = existing_val
+    let original_datasets = existing_val
         .and_then(Datasets::from_slice)
         .unwrap_or_default();
+    let mut datasets = original_datasets.clone();
 
     for op in operands {
         let new_vals = Datasets::from_slice(op).unwrap();
         datasets = Datasets(datasets.0.union(&new_vals.0).cloned().collect());
     }
+    //    if let Some(_) = datasets.0.difference(&original_datasets.0).next() {
     datasets.as_bytes()
+    //    } else {
+    //        None
+    //    }
 }
 
-#[derive(Default, Debug, PartialEq, Archive, Serialize, Deserialize)]
+fn map_hashes_colors(
+    db: Arc<DB>,
+    dataset_id: DatasetID,
+    search_sig: &Signature,
+    threshold: usize,
+    template: &Sketch,
+    //) -> Option<(HashToColor, Datasets)> {
+) {
+    let mut search_mh = None;
+    if let Some(Sketch::MinHash(mh)) = search_sig.select_sketch(template) {
+        search_mh = Some(mh);
+    }
+
+    let search_mh = search_mh.expect("Couldn't find a compatible MinHash");
+    let colors = Datasets::new(&[dataset_id]).as_bytes().unwrap();
+
+    let matched = search_mh.mins();
+    let size = matched.len() as u64;
+    if !matched.is_empty() || size > threshold as u64 {
+        let mut hash_bytes = [0u8; 8];
+        for hash in matched {
+            (&mut hash_bytes[..])
+                .write_u64::<LittleEndian>(hash)
+                .expect("error writing bytes");
+            db.merge(&hash_bytes[..], colors.as_slice())
+                .expect("error merging");
+        }
+    }
+
+    /*
+        if hash_to_color.is_empty() {
+            None
+        } else {
+            Some((hash_to_color, colors))
+        }
+    */
+}
+
+#[derive(Default, Debug, PartialEq, Clone, Archive, Serialize, Deserialize)]
 struct Datasets(HashSet<DatasetID>);
 
 impl Datasets {
@@ -37,22 +81,45 @@ impl Datasets {
     }
 
     fn from_slice(slice: &[u8]) -> Option<Self> {
-        let mut deserializer = DefaultDeserializer::default();
-        let archived_value = unsafe { archived_root::<Datasets>(slice) };
-        let inner = archived_value.deserialize(&mut deserializer).unwrap();
+        // TODO: avoid the aligned vec allocation here
+        let mut vec = rkyv::AlignedVec::new();
+        vec.extend_from_slice(slice);
+        let archived_value = unsafe { rkyv::archived_root::<Datasets>(vec.as_ref()) };
+        let inner = archived_value.deserialize(&mut rkyv::Infallible).unwrap();
         Some(inner)
     }
 
     fn as_bytes(&self) -> Option<Vec<u8>> {
+        let bytes = rkyv::to_bytes::<_, 256>(self).unwrap();
+        Some(bytes.into_vec())
+
+        /*
         let mut serializer = DefaultSerializer::default();
-        serializer.serialize_value(self).unwrap();
+        let v = serializer.serialize_value(self).unwrap();
+        debug_assert_eq!(v, 0);
         let buf = serializer.into_serializer().into_inner();
+        debug_assert!(Datasets::from_slice(&buf.to_vec()).is_some());
         Some(buf.to_vec())
+        */
     }
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let n = Path::new("_rocksdb_sourmash_test");
+
+    let max_hash = max_hash_for_scaled(10000);
+    let template = Sketch::MinHash(
+        KmerMinHash::builder()
+            .num(0u32)
+            .ksize(31)
+            .max_hash(max_hash)
+            .build(),
+    );
+    let search_sigs: [PathBuf; 3] = [
+        "tests/test-data/GCF_000006945.2_ASM694v2_genomic.fna.gz.sig".into(),
+        "tests/test-data/GCF_000007545.1_ASM754v1_genomic.fna.gz.sig".into(),
+        "tests/test-data/GCF_000008105.1_ASM810v1_genomic.fna.gz.sig".into(),
+    ];
 
     let mut opts = Options::default();
     opts.create_if_missing(true);
@@ -62,17 +129,59 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     {
         let db = Arc::new(DB::open(&opts, &n).unwrap());
 
-        let p = db.put(b"k1", Datasets::new(&[1]).as_bytes().unwrap())?;
-        db.merge(b"k1", Datasets::new(&[2]).as_bytes().unwrap())?;
-        db.merge(b"k1", Datasets::new(&[3]).as_bytes().unwrap())?;
-        db.merge(b"k1", Datasets::new(&[4, 5, 6]).as_bytes().unwrap())?;
+        let threshold = 0;
 
-        let r = db.get(b"k1")?;
+        let processed_sigs = AtomicUsize::new(0);
+        //let sig_iter = search_sigs.par_iter();
+        let sig_iter = search_sigs.iter();
+
+        let _filtered_sigs = sig_iter
+            .enumerate()
+            .filter_map(|(dataset_id, filename)| {
+                let i = processed_sigs.fetch_add(1, Ordering::SeqCst);
+                if i % 1000 == 0 {
+                    eprintln!("Processed {} reference sigs", i);
+                }
+
+                let search_sig = Signature::from_path(&filename)
+                    .unwrap_or_else(|_| panic!("Error processing {:?}", filename))
+                    .swap_remove(0);
+
+                map_hashes_colors(
+                    db.clone(),
+                    dataset_id as DatasetID,
+                    &search_sig,
+                    threshold,
+                    &template,
+                );
+                Some(true)
+            })
+            .count();
+
+        eprintln!("Processed {} reference sigs", processed_sigs.into_inner());
+
+        /*
+        let mut hash_bytes = [0u8; 8];
+        (&mut hash_bytes[..])
+            .write_u64::<LittleEndian>(1078036129600)
+            .expect("error writing bytes");
+        let r = db.get(&hash_bytes[..])?;
         assert_eq!(
             Datasets::from_slice(&r.unwrap()).unwrap(),
             Datasets::new(&[1, 2, 3, 4, 5, 6])
         );
+        */
+
+        /*
+        use byteorder::ReadBytesExt;
+        let mut iter = db.iterator(rocksdb::IteratorMode::Start); // Always iterates forward
+        for (key, value) in iter {
+            let k = (&key[..]).read_u64::<LittleEndian>().unwrap();
+            println!("Saw {} {:?}", k, Datasets::from_slice(&value));
+            //println!("Saw {} {:?}", k, value);
+        }
+        */
     }
-    let _ = DB::destroy(&opts, n);
+    //let _ = DB::destroy(&opts, n);
     Ok(())
 }
